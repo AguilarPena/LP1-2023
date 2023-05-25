7b079ba1b3c800e0ce183abedb64e8014d4669f8
AguilarPena <126773631+AguilarPena@users.noreply.github.com>
2023-05-25 09:10:29 -0300

    Add files via upload

---
+++ 2º Bimestre/Exemplos/Hoje/src/Funcionarios.java
@@ -0,0 +1,25 @@
+public class Funcionarios{
+    private String nome;
+    private int CPF;
+
+    public Funcionarios(String nome, int CPF) {
+        this.nome = nome;
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+}
