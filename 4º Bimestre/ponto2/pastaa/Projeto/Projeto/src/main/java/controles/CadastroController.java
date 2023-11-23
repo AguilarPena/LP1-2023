ea6ca503e94bfe08ce588cdb8c347411cd0b5c33
Emanuel Aguilar <126773631+AguilarPena@users.noreply.github.com>
2023-11-23 17:29:07 -0300

    Add files via upload

---
+++ 4º Bimestre/ponto2/pastaa/Projeto/Projeto/src/main/java/controles/CadastroController.java
@@ -0,0 +1,101 @@
+package controles;
+import GerenciadorDeTarefas.projeto.Application;
+import modelos.Usuario;
+import sessoes.UsuarioSessao;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+
+public class CadastroController {
+
+    @FXML
+    public Button novoUsuario;
+
+    @FXML
+    public TextField Cnome;
+
+    @FXML
+    public TextField Cemail;
+
+    @FXML
+    public TextField Csenha;
+
+    @FXML
+    public TextField senhaConf;
+
+    @FXML
+    public Label mensagem;
+
+    UsuarioSessao usuarioSessao = new UsuarioSessao();
+
+    /*if(Cnome.isEmpty() || Cnome.isEmpty(0 || Csenha.isEmpty())){
+        exibirErro("Usuário já existe. Entre com outro nome e usuário");
+    } else {
+        usuarioService.salvarUsuario(novoUsuario);
+        GerenciadorApplication.mudarTela("login");
+    }
+
+    public void novoUsuario() {
+        boolean senhaIgual  = senhaConf.getText().equals(Csenha.getText());
+
+        if(!senhaIgual) {
+            return;
+        }
+
+        String senhaBdd = Csenha.getText();
+        String usuarioBdd = Cnome.getText();
+        String emailbdd = Cemail.getText();
+
+        Usuario usuarioA = new Usuario(usuarioBdd,emailbdd, senhaBdd);
+
+        System.out.println("Cadastrado!");
+    }*/
+
+    public void mudaLogin(ActionEvent event) {
+
+        String nome = Cnome.getText();
+        String usuario = Cemail.getText();
+        String senha = Csenha.getText();
+
+        if(nome.isEmpty() && usuario.isEmpty() && senha.isEmpty())
+            exibirErro(0);
+
+        else  if(nome.isEmpty())
+            exibirErro(1);
+
+        else if(usuario.isEmpty())
+            exibirErro(2);
+
+        else if(senha.isEmpty())
+            exibirErro(3);
+
+        else
+            Usuario u = new Usuario(nome, email, senha);
+            UsuarioSessao.salvarUsuario(u);
+            Application.mudartela("login");
+    }
+
+    public void retornar(ActionEvent event) {
+        Application.mudartela("login");
+    }
+
+    public void exibirErro(int i) {
+        if(i==0)
+            mensagem.setText("Campos vazios");
+
+        else if(i==1)
+            mensagem.setText("Campo nome está vazio");
+
+        else if(i==2)
+            mensagem.setText("Campo usuário está vazio");
+
+        else if(i==3)
+            mensagem.setText("Campo Senha está vazio");
+
+    }
+
+}
+
+
---
+++ 4º Bimestre/ponto2/pastaa/Projeto/Projeto/src/main/java/controles/LoginController.java
@@ -0,0 +1,123 @@
+package controles;
+
+import GerenciadorDeTarefas.projeto.Application;
+import GerenciadorDeTarefas.projeto.util.HibernateUtil;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import modelos.Usuario;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class LoginController {
+    @FXML
+    private TextField Lsenha;
+
+    @FXML
+    private TextField Lemail;
+
+    @FXML
+    private Label mensagem;
+
+    @FXML
+    private Button Lentra;
+
+    @FXML
+    private Button Lcadastro;
+
+    @FXML
+    private Button Vsenha;
+
+    @FXML
+    private Button Vuser;
+
+
+    //ao clicar no botão cadastrar o usuario é mandado para a tela de cadastro
+    @FXML
+    void buttonCadastrar(ActionEvent event){
+        Application.mudartela("cadastro");
+    }
+
+    public void verificacaoUser(ActionEvent actionEvent) {
+        String email = Lemail.getText();
+        String senha = Lsenha.getText();
+
+        if(email.isEmpty() && senha.isEmpty())
+            exibirErro(0);
+
+        else  if(email.isEmpty())
+            exibirErro(1);
+
+        else if(senha.isEmpty())
+            exibirErro(2);
+
+        else{
+
+            String hql = "FROM Usuario u WHERE u.login = :login AND u.senha = :senha";
+            Query<Usuario> query = HibernateUtil.getSessionFactory().openSession().createQuery(hql, Usuario.class);
+            query.setParameter("email", Lemail.getText());
+            query.setParameter("senha", Lsenha.getText());
+
+            List<Usuario> usuarios = query.getResultList();
+
+            if (!usuarios.isEmpty()) {
+
+                Application.mudartela("principal");
+            } else {
+                System.out.println("usuário não está registrado no banco.");
+                exibirErro(3);
+            }
+        }
+    }
+
+
+    public void exibirErro(int i) {
+        if (i == 0)
+            mensagem.setText("Campos  vazios !!");
+
+        else if (i == 1)
+            mensagem.setText("Campo nome vazio !!");
+
+        else if (i == 2)
+            mensagem.setText("Campo Senha vazio !!");
+        else
+            mensagem.setText("inválido!!");
+    }
+
+    protected Boolean verificacaoSenha(){
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        try {
+            Usuario usuario = session.createQuery("from Usuario where senha = :senha", Usuario.class)
+                    .setParameter("senha", Lsenha.getText())
+                    .uniqueResult();
+
+            if(usuario == null){
+                Vsenha.setText("Senha incorreta!");
+                return false;
+            }
+
+            //System.out.println(usuario);
+            //transaction.commit();
+            //Application.mudartela("principal");
+
+        session.getTransaction().commit();
+        } catch(Exception e) {
+            if(session.getTransaction() != null)
+                session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return true;
+    }
+
+    protected void limpar(){
+        Vsenha.setText("");
+        Vuser.setText("");
+    }
+
+}
